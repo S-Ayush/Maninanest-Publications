7fa37f223af2276881a8d98d2a9218fb4adb11a4
S-Ayush <ayushshrivastava076@gmail.com>
2022-01-14 02:31:22 -0800

    changes in Jwt Token Login

---
+++ apps/js/dashboard.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function () {
+  const API_HOST = "http://localhost:3000";
+  const getUserData = async (data) => {
+    const res = await fetch(`${API_HOST}/userData`, {
+      method: "GET",
+      headers: {
+        Accept: "*/*",
+        "content-Type": "application/json",
+      },
+      credentials: "include",
+      cookies: document.cookie,
+    });
+    if (res.status === 401) {
+      document.location = "/";
+    }
+  };
+  getUserData();
+});
